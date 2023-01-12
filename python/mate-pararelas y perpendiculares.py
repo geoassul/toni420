5924e2e584de1d8907b954c491e5800f5c49d8c0
anthony <anthony.assul@gmail.com>
2023-01-12 10:37:59 -0500

    borrando codigo errado

--- python/mate-pararelas y perpendiculares.py
+++
@@ -1,9 +0,0 @@
-# consulta si dos rectas con perpendiculares
-x = 'objeto'
-class perpendicular:
-    def direccion(rectaA,rectaB):
-        if abs(rectaA-rectaB) == 90:
-            print('es perpendicular')
-        else:
-            print('no es perpendicular')
-    
